66c9405be72dd135a08eaa4773fdcd9b55e13426
*Pamela <paulinopamela@protonmail.com>
2021-09-30 11:10:39 -0300

    rotas entrar e cadastrar

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin{
+    public id: number
+    public nome: string
+    public username: string
+    public senha: string
+    public token: string
+    public dataNascimento: Date
+    public foto: string
+    public tipo: string
+}
