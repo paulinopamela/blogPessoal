e982688c3836a2e708a3248172e0133770e930a4
*Pamela <paulinopamela@protonmail.com>
2021-10-02 13:41:07 -0300

    consumo api

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+  }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.username = this.userLogin.username
+      environment.id = this.userLogin.id
+      environment.foto = this.userLogin.foto
+
+      /*
+      console.log(environment.token)
+      console.log(environment.username)
+      console.log(environment.id)
+      console.log(environment.foto)
+      */
+
+      this.router.navigate (['/inicio'])
+    }, erro => {
+      if(erro.status == 500){
+        alert('Usuário ou senha incorretos!')
+      }
+    })
   }
 
 }
